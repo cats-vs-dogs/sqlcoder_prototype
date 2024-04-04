8690cbbd772b0bf06473d30177f12aa026f10bad
Pavel <pavel170@yahoo.com>
2024-04-04 16:09:31 +0300

    rwa tool

--- app.py
+++ app.py
@@ -15,9 +15,44 @@ from langchain.prompts import PromptTemplate, load_prompt
 from langchain.agents.agent import AgentExecutor
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+from pydantic import BaseModel, Field
 from slimify import *
 import re
 
+
+def rw_corp(Input_String: str) -> str:
+
+    """ 
+    calculates the risk weight from input parameters 
+    PD, LGD, MATURITY, F_LARGE_FIN and SIZE which are fed into the formula in this particular order 
+    """
+    
+    PD_s, LGD_s, MATURITY_s, SIZE_s, F_LARGE_FIN_s = Input_String.split(",")
+
+    PD = float(eval(PD_s.split(":")[1].strip()))
+    LGD = float(eval(LGD_s.split(":")[1].strip()))
+    MATURITY = float(eval(MATURITY_s.split(":")[1].strip()))
+    SIZE = float(eval(SIZE_s.split(":")[1].strip()))
+    F_LARGE_FIN = (F_LARGE_FIN_s.split(":")[1].strip())
+       
+    pd_final = max(0.0003, PD)
+    size_final = max(5, min(SIZE, 50))    
+    r0 = (0.12 * (1.0 - math.exp(-50.0 * pd_final)) / (1.0 - math.exp(-50.0))) + \
+        (0.24 * (1.0 - (1 - math.exp(-50.0 * pd_final)) / (1.0 - math.exp(-50.0)))) - \
+        (0.04 * (1.0 - (size_final - 5.0) / 45.0))
+    if F_LARGE_FIN == 'Y':
+        r = r0 * 1.25
+    else:
+        r = r0
+    b = (0.11852 - 0.05478 * math.log(pd_final)) ** 2
+    ma = ((1 - 1.5 * b) ** -1) * (1 + (MATURITY - 2.5) * b)    
+    rw = ((LGD * norm.cdf((1 - r) ** -0.5 * norm.ppf(pd_final) + (r / (1 - r)) ** 0.5 * norm.ppf(0.999)) - pd_final * LGD) * ma) * 12.5 * 1.06
+    return rw  
+
+class RWInput(BaseModel):
+    Input_String: str = Field(description='This is a string that contains values for the input parameters \
+                              PD, LGD, MATURITY, SIZE and F_LARGE_FIN which are fed into the formula in this particular order ')
+    
 
 def fix_query(query):
     r"""
@@ -94,6 +129,20 @@ vector_db = initialize_vectorstore(few_shots)
 db = SQLDatabase.from_uri("sqlite:///./portfolio_data.db",
                          sample_rows_in_table_info=2)
 toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI())
+RWTool = Tool.from_function(
+    func=rw_corp,
+    name="RWTool",
+    description="""
+    This is a custom tool that calculates the risk weight from a set of input parameters:
+        PD - Probability of Default,
+        LGD - Loss Given Default,
+        MATURITY - Remaining maturity of the loan in years,
+        SIZE - The size of the client in MEUR, usually this is the client's turnover, 
+        F_LARGE_FIN - If 'Y' the client is a Large Financial Institution        
+        """,
+    # args_schema=RWInput
+)
+
 # tools = [
 #     Tool.from_function(
 #         func=generate_sql_query,
@@ -105,7 +154,7 @@ toolkit = SQLDatabaseToolkit(db=db, llm=OpenAI())
 # sql_tools = toolkit.get_tools()
 # sql_tools.pop(1)
 # tools = tools+sql_tools
-tools = toolkit.get_tools()
+tools = toolkit.get_tools() + [RWTool]
 
 memory = ConversationBufferWindowMemory(k=4, memory_key="history")
 
